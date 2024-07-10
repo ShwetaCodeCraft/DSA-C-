5665e193d873a77000a634d4503c58890becd03d
Shweta Singh <singh.shweta2973@gmail.com>
2024-07-10 20:33:48 +0530

    arr-1(Largest elem)

---
+++ p30.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+// git init
+// git add .
+// git add -A
+// git commit -m "arr-1(Largest elem)"
+// git push origin master
+int largest(const vector<int> &arr, int n) {
+    int largest = arr[0];
+    for (int i = 1; i < n; i++) {
+        if (arr[i] > largest) {
+            largest = arr[i];
+        }
+    }
+    return largest;
+}
+
+int main() {
+        int n;
+        cin >> n; // Size of array
+        vector<int> arr(n);
+        for (int i = 0; i < n; i++) {
+            cin >> arr[i]; // Read array elements
+        }
+        cout << largest(arr, n) << "\n"; // Print the largest element
+    return 0;
+}
