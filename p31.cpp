5e9f88f7c5aeb70840fc92811f2289b49780c57b
Shweta Singh <singh.shweta2973@gmail.com>
2024-07-12 00:55:07 +0530

    arr-2(secongLargest elem)

---
+++ p31.cpp
@@ -0,0 +1,42 @@
+//Arrays -2
+//second Largest element in an array
+
+// git init
+// git add .
+// git add -A
+// git commit -m "arr-2(secongLargest elem)"
+// git push origin master
+
+#include <bits/stdc++.h>
+using namespace std;
+    int print2largest(vector<int> &arr) {
+        int slargest =-1;
+        int largest = arr[0];
+        for(int i= 1; i< arr.size(); i++){
+            if(arr[i]>largest){
+                slargest=largest;
+                largest=arr[i];
+            }
+            else if(arr[i]<largest && arr[i] > slargest){
+                slargest = arr[i];
+            }
+        }
+        return slargest;
+    }
+
+
+int main() {
+    int t;
+    cin >> t;
+        vector<int> arr;
+        string input;
+        getline(cin, input);
+        stringstream ss(input);
+        int number;
+        while (ss >> number) {
+            arr.push_back(number);
+        }
+        int ans = print2largest(arr);
+        cout << ans << endl;
+    return 0;
+}
