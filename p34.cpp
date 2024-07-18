622c3227ba1dedaf58b06caf6584a53c4f6c5049
Shweta Singh <singh.shweta2973@gmail.com>
2024-07-18 15:34:51 +0530

    arr-5(Left Rotate the Array by One)

--- p34.cpp
+++ p34.cpp
@@ -4,13 +4,13 @@
 #include<bits/stdc++.h>
 using namespace std;
 void solve(int arr[], int n) {
-  int temp[n];
-  for (int i = 1; i < n; i++) {
-    temp[i - 1] = arr[i];
-  }
-  temp[n - 1] = arr[0];
+  int temp=arr[0];
   for (int i = 0; i < n; i++) {
-    cout << temp[i] << " ";
+    arr[i] = arr[i+1];
+  }
+  arr[n-1]=temp;
+  for (int i = 0; i < n; i++) {
+    cout << arr[i] << " ";
   }
   cout << endl;
 }
